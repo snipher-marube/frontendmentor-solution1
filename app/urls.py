bfd5883d6f29166d2deee7e818dfd5ddd45628ef
snipher marube <sniphermarube@gmail.com>
2023-08-10 18:37:45 +0300

    pricing, addons and summary steps pages  done

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('pricing/', views.pricing, name='pricing'),
+    path('addons/', views.addons, name='addons'),
+    path('summary/', views.summary, name='summary'),
 ]
--- app/views.py
+++ app/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'app/home.html')
+
+def pricing(request):
+    return render(request, 'app/pricing.html')
+
+def addons(request):
+    return render(request, 'app/addons.html')
+
+def summary(request):
+    return render(request, 'app/summary.html')
