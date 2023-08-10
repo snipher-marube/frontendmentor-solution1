ab11e6e2ba9f8736e7ad402390dccbda991ca78e
snipher marube <sniphermarube@gmail.com>
2023-08-10 09:47:33 +0300

    configuring tailwind first

---
+++ multi_step_form_main/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for multi_step_form_main project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'multi_step_form_main.settings')
+
+application = get_asgi_application()
