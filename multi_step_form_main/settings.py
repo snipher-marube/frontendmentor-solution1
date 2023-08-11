4d24d07af4888429e09b21481a889cb082c1aca0
snipher marube <sniphermarube@gmail.com>
2023-08-11 12:37:06 +0300

    allowed hosts

--- multi_step_form_main/settings.py
+++ multi_step_form_main/settings.py
@@ -14,7 +14,9 @@ SECRET_KEY = 'django-insecure--s6cr2%ic$58__98yr_0s_c1)#4@^ypmzlw#xcq(sb@26h14kb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '*'
+]
 
 
 # Application definition
